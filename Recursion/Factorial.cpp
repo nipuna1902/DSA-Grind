df584d2d039f730547b35ded3d78a5a85784ed94
Nipuna <nipunamahakur@gmail.com>
2025-09-17 14:54:29 +0530

    Factorial of N numbers

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int a)
+{
+  if (a == 0 || a == 1)
+    return 1;
+  else
+    return a * factorial(a - 1);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  cout << "The factorial is" << factorial(n) << endl;
+  return 0;
+}

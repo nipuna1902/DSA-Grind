c2bd5484259e7e859bdef582638f98c2595e9fab
Nipuna <nipunamahakur@gmail.com>
2025-11-15 21:51:35 +0530

    Sort an array of 0's 1's and 2's

---
+++ 06-Array/Easy/0-1-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, low = 0, mid = 0;
+  cout << "Enter size of array: ";
+  cin >> n;
+  int arr[n];
+  int high = n - 1;
+  cout << "Enter elements: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  while (mid <= high)
+  {
+    if (arr[mid] == 0)
+    {
+      swap(arr[low], arr[mid]);
+      low++;
+      mid++;
+    }
+    else if (arr[mid] == 1)
+    {
+      mid++;
+    }
+    else
+    {
+      swap(arr[mid], arr[high]);
+      high--;
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i];
+  }
+}

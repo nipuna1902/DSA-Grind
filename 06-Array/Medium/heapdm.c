0585069435892dd2f92d56e4b5bf6fe4ddcfad6a
Nipuna <nipunamahakur@gmail.com>
2026-01-18 12:25:57 +0530

    Binary Search to find X in sorted array

---
+++ 06-Array/Medium/heapdm.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void heapify(int a[], int n, int i)
+{
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+  if (left < n && a[left] > a[largest])
+    largest = left;
+  if (right < n && a[right] > a[largest])
+    largest = right;
+  if (largest != i)
+  {
+    int temp = a[i];
+    a[i] = a[largest];
+    a[largest] = temp;
+    heapify(a, n, largest);
+  }
+}
+
+void heapSort(int a[], int n)
+{
+
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapify(a, n, i);
+  for (int i = n - 1; i >= 0; i--)
+  {
+    int temp = a[0];
+    a[0] = a[i];
+    a[i] = temp;
+    heapify(a, i, 0);
+  }
+}
+
+int main()
+{
+  int a[] = {50, 20, 60, 70, 40, 90, 100, 10, 30, 80};
+  int n = 10;
+
+  heapSort(a, n);
+
+  printf("Sorted array: ");
+  for (int i = 0; i < n; i++)
+    printf("%d ", a[i]);
+
+  return 0;
+}
---
+++ 07-Binary-Search/1-D-array/Findx.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int X)
+{
+  int low = 0, high = n - 1;
+
+  while (low <= high)
+  {
+    int mid = low + (high - low) / 2; // safe mid
+
+    if (arr[mid] == X)
+      return mid; // found
+
+    else if (arr[mid] < X)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1; // not found
+}
+
+int main()
+{
+  int n, X;
+  cout << "Enter size of sorted array: ";
+  cin >> n;
+
+  int arr[n];
+  cout << "Enter sorted elements: ";
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  cout << "Enter element to search: ";
+  cin >> X;
+
+  int index = binarySearch(arr, n, X);
+
+  if (index != -1)
+    cout << "Element found at index " << index << endl;
+  else
+    cout << "Element not found" << endl;
+
+  return 0;
+}

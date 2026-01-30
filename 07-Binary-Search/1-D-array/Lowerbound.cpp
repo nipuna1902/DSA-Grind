413d6b1df7ee6caa851de6f59b3b138a15efeaaf
Nipuna <nipunamahakur@gmail.com>
2026-01-30 09:03:35 +0530

    Implement Lower Bound

---
+++ 07-Binary-Search/1-D-array/Lowerbound.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int lowerbound(int arr[], int n, int x)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+
+  while (low <= high)
+  {
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] >= x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+int main()
+{
+  int n, X;
+  cout << "Enter size of sorted array: ";
+  cin >> n;
+
+  int arr[n];
+  cout << "Enter sorted elements: ";
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  cout << "Enter element to be inserted: ";
+  cin >> X;
+
+  int index = lowerbound(arr, n, X);
+  cout << X << " should be at index " << index;
+}

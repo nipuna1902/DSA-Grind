d424151b7cbf213ef8871fe01737fea839875c1d
Nipuna Mahakur <nipunamahakur@gmail.com>
2026-07-01 18:02:47 +0530

    Time: 16 ms (15.43%), Space: 14 MB (91.67%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -19,3 +19,32 @@ public:
         return ans;
     }
 };
+// class Solution {
+// public:
+//     long long subArrayRanges(vector<int>& nums) {
+//         int n = nums.size();
+//         long long ans = 0;
+//         stack<int> st;
+//         for (int i = 0; i <= n; i++) {
+//             while (!st.empty() && (i == n || nums[st.top()] < nums[i])) {
+//                 int mid = st.top();
+//                 st.pop();
+//                 int left = st.empty() ? -1 : st.top();
+//                 ans += 1LL * nums[mid] * (mid - left) * (i - mid);
+//             }
+//             st.push(i);
+//         }
+
+//         while (!st.empty()) st.pop();
+//         for (int i = 0; i <= n; i++) {
+//             while (!st.empty() && (i == n || nums[st.top()] > nums[i])) {
+//                 int mid = st.top();
+//                 st.pop();
+//                 int left = st.empty() ? -1 : st.top();
+//                 ans -= 1LL * nums[mid] * (mid - left) * (i - mid);
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
+// };

3d7d8002066321e4e83d5c26391a8ffd93694ba2
Nipuna Mahakur <nipunamahakur@gmail.com>
2026-08-18 09:14:55 +0530

    Time: 0 ms (100%), Space: 28.8 MB (88.72%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        for (int i = 0; i <= nums.size() - k; i++) {
-            unordered_set<int> seen;
-            for (int j = i; j < i + k; j++) {
-                seen.insert(nums[j]);
-            }
-            for (int num : seen) {
+        int n = nums.size();
+        if (k == 1) {
+            unordered_map<int, int> count;
+            for (int num : nums) {
                 count[num]++;
             }
+            int ans = -1;
+            for (auto &[num, freq] : count) {
+                if (freq == 1) {
+                    ans = max(ans, num);
+                }
+            }
+
+            return ans;
+        }
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        int first = nums[0];
+        int last = nums[n - 1];
+        int firstCount = 0;
+        int lastCount = 0;
+        for (int num : nums) {
+            if (num == first) firstCount++;
+            if (num == last) lastCount++;
         }
         int ans = -1;
-        for (auto &[num, freq] : count) {
-            if (freq == 1) {
-                ans = max(ans, num);
-            }
+        if (firstCount == 1) {
+            ans = max(ans, first);
+        }
+        if (lastCount == 1) {
+            ans = max(ans, last);
         }
         return ans;
     }

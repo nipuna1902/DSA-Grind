2391fa12486007ad565b762bc5a1881c7c763540
Nipuna Mahakur <nipunamahakur@gmail.com>
2026-08-20 20:19:14 +0530

    Time: 78 ms (5.78%), Space: 34.3 MB (11.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs){
+            vector<int> count(26,0);
+            for(char c:s){
+                count[c-'a']++;
+            }
+            string key="";
+            for(int x:count){
+                key+=to_string(x)+"#";
+            }
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto& it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};

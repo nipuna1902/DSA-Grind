b0dd05190e257252c562c0ad1976199b364cc9f7
Nipuna Mahakur <nipunamahakur@gmail.com>
2026-08-27 13:27:45 +0530

    Time: 26 ms (62.93%), Space: 23.5 MB (25.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,69 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int r,int c){
-        if(r<0||c<0||r>=grid.size()||c>=grid[0].size()){
-            return;
-        }
-        if(grid[r][c]=='0')
-        return;
-        grid[r][c]='0';
-        dfs(grid,r-1,c);
-        dfs(grid,r+1,c);
-        dfs(grid,r,c-1);
-        dfs(grid,r,c+1);
-    }
     int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
         int count=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]=='1'){
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
                     count++;
-                    dfs(grid,i,j);
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    vis[i][j]=true;
+                    while(!q.empty()){
+                        int x=q.front().first;
+                        int y=q.front().second;
+                        q.pop();
+                        if(x-1>=0 && grid[x-1][y]=='1' && !vis[x-1][y]){
+                            vis[x-1][y]=true;
+                            q.push({x-1,y});
+                        }
+                        if(y+1<m && grid[x][y+1]=='1' && !vis[x][y+1]){
+                            vis[x][y+1]=true;
+                            q.push({x,y+1});
+                        }
+                        if(x+1<n && grid[x+1][y]=='1' && !vis[x+1][y]){
+                            vis[x+1][y]=true;
+                            q.push({x+1,y});
+                        }
+                        if(y - 1 >= 0 && grid[x][y-1] == '1' && !vis[x][y-1]) {
+                            vis[x][y-1] = true;
+                            q.push({x, y-1});
+                        }
+                    }
                 }
             }
         }
         return count;
     }
 };
+
+// class Solution {
+// public:
+//     void dfs(vector<vector<char>>& grid,int r,int c){
+//         if(r<0||c<0||r>=grid.size()||c>=grid[0].size()){
+//             return;
+//         }
+//         if(grid[r][c]=='0')
+//         return;
+//         grid[r][c]='0';
+//         dfs(grid,r-1,c);
+//         dfs(grid,r+1,c);
+//         dfs(grid,r,c-1);
+//         dfs(grid,r,c+1);
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         int count=0;
+//         for(int i=0;i<grid.size();i++){
+//             for(int j=0;j<grid[0].size();j++){
+//                 if(grid[i][j]=='1'){
+//                     count++;
+//                     dfs(grid,i,j);
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };

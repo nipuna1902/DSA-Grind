06c587fe5327b6b72f823a791d37501eec84527d
Nipuna Mahakur <nipunamahakur@gmail.com>
2026-09-01 19:08:19 +0530

    Time: 195 ms (72.49%), Space: 10.5 MB (64.78%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
+    bool dfs(vector<vector<char>> &board,string& word,int i,int j,int count){
+        if(count==word.length())
+        return true;
+        if (i < 0 || i >= board.size() ||
+            j < 0 || j >= board[0].size() ||
+            board[i][j] != word[count]) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+        bool found =
+            dfs(board, word, i - 1, j, count + 1) ||
+            dfs(board, word, i + 1, j, count + 1) ||
+            dfs(board, word, i, j - 1, count + 1) ||
+            dfs(board, word, i, j + 1, count + 1);
+        board[i][j] = temp;
+        return found;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(dfs(board, word, i, j, 0))
-                    return true;
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
+                    if(dfs(board,word,i,j,0)){
+                        return true;
+                    }
+                }
             }
         }
         return false;
-    }
-    bool dfs(vector<vector<char>> &board,string &word, int row,int col, int idx){
-        if(idx==word.size())
-            return true;
-             if(row < 0 || col < 0 ||
-           row >= board.size() || col >= board[0].size() ||
-           board[row][col] != word[idx])
-            return false;
-
-            char temp = board[row][col];
-            board[row][col] = '#';
-            bool found =
-            dfs(board, word, row + 1, col, idx + 1) ||
-            dfs(board, word, row - 1, col, idx + 1) ||
-            dfs(board, word, row, col + 1, idx + 1) ||
-            dfs(board, word, row, col - 1, idx + 1);
-
-        board[row][col] = temp;
-
-        return found;
     }
 };
